d3fa95ae9cfa103ab49411f6f1bc43834674b0f2
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2024-11-25 20:40:02 -0800

    Auto-sync from slvpi4-2: Mon 25 Nov 20:40:02 PST 2024

--- native-pwm/servo3.py
+++ native-pwm/servo3.py
@@ -1,18 +1,20 @@
 from gpiozero import PWMOutputDevice
 import time
 
-pwm = PWMOutputDevice(18)
+pwm = PWMOutputDevice(18,frequency=50)
 
 try:
     for _ in range(5):  # Repeat 5 times
         # Ramp up
-        for value in [x/100 for x in range(0, 101, 10)]:
+        for value in [x/100 for x in range(0, 21, 10)]:
             pwm.value = value
-            time.sleep(0.1)
+            print(value)
+            time.sleep(0.5)
         
         # Ramp down
         for value in [x/100 for x in range(100, -1, -10)]:
             pwm.value = value
+            print(value)
             time.sleep(0.1)
 
 except KeyboardInterrupt:
