bee600c68ed5d83340dd346116049e911f9421cc
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2024-11-25 20:55:02 -0800

    Auto-sync from slvpi4-2: Mon 25 Nov 20:55:02 PST 2024

--- native-pwm/servo3.py
+++ native-pwm/servo3.py
@@ -6,10 +6,10 @@ pwm = PWMOutputDevice(18,frequency=50)
 try:
     for _ in range(5):  # Repeat 5 times
         # Ramp up
-        for value in [x/100 for x in range(0, 21, 10)]:
+        for value in [x/100 for x in range(60, 101, 10)]:
             pwm.value = value
             print(value)
-            time.sleep(0.5)
+            time.sleep(2)
         
         # Ramp down
         for value in [x/100 for x in range(100, -1, -10)]:
