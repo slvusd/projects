b4a450bc658130e8fa5b0735f70393d7b9771dec
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2024-11-26 13:25:02 -0800

    Auto-sync from slvpi3-1: Tue 26 Nov 13:25:02 PST 2024

---
+++ variable_motor/control1.py
@@ -0,0 +1,42 @@
+import time
+import board
+import busio
+import sys
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_servokit import ServoKit
+
+# Initialize PCA9685 with 16 channels
+kit = ServoKit(channels=16)
+
+# Set the ESC control channel (e.g., channel 0)
+esc_channel = 0
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c, gain=1)
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P3)
+
+# Create differential input between channel 0 and 1
+#chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+print("{:>5}\t{:>5}".format('raw', 'v'))
+
+while True:
+    # voltage goes from 0 to 4.1 with 2.5 being the middle
+    if chan.voltage > 2.6:
+        v = (2.6 - chan.voltage) / (4.1-2.6)
+        a = 100 + 40 * v
+    elif chan.voltage < 2.4:
+        v = chan.voltage / 2.4
+        a = 80 - 40 * v
+    else:
+        a = 90
+    
+    kit.servo[0].angle = a
+    print("{:>5}\t{:>5.3f}\t{:>5}".format(chan.value, chan.voltage, a))
+    time.sleep(0.5)
