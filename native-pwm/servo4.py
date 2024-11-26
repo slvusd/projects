194dee75e99324fc42a26008e84812dba02ebaf1
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2024-11-25 21:05:02 -0800

    Auto-sync from slvpi4-2: Mon 25 Nov 21:05:02 PST 2024

---
+++ native-pwm/servo4.py
@@ -0,0 +1,24 @@
+from gpiozero import PWMOutputDevice
+import time
+
+pwm = PWMOutputDevice(18,frequency=50)
+
+try:
+    for _ in range(5):  # Repeat 5 times
+        # Ramp up
+        for value in [x/100 for x in range(60, 101, 10)]:
+            pwm.value = value
+            print(value)
+            time.sleep(2)
+        
+        # Ramp down
+        for value in [x/100 for x in range(100, -1, -10)]:
+            pwm.value = value
+            print(value)
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    pwm.close()
+
