6010ef3d16992fc61d523794a894a32be666ef36
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2024-12-25 13:55:01 -0800

    Auto-sync from slvpi5: Wed 25 Dec 13:55:01 PST 2024

---
+++ timelapse/snapshot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+libcamera-jpeg -n -o - |\
+      convert - -distort SRT 183 -resize 1920x \
+      	-gravity southeast -pointsize 24 -fill white -annotate +10+10 "$(date +'%Y-%m-%d %H:%M:%S')" \
+	$HOME/images/img$(date +'%Y%m%dT%H%M%S').jpg
+
