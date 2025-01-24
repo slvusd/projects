d4133468b26dcfcdd5d8a712a3153846b0def7d8
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2025-01-24 14:45:06 -0800

    Auto-sync from slvpi4-2: Fri 24 Jan 14:45:04 PST 2025

--- vent/app.py
+++ vent/app.py
@@ -3,6 +3,7 @@ from gpiozero import LED
 from time import sleep
 from board import I2C
 from adafruit_ahtx0 import AHTx0
+from threading import Lock
 
 app = Flask(__name__)
 
@@ -11,24 +12,37 @@ i2c = I2C()
 thermometer = AHTx0(i2c)
 temper = thermometer.temperature
 
+def control(command):
+	mutex = Lock()
+	mutex.aquire()
+
+	pin17 = LED(17)
+	pin27 = LED(27)
+
+	if command == 1:
+		pin17.on()
+		print("pin17 (open) trigger")
+		sleep(5)
+	elif command == -1:
+		pin27.on()
+		print("pin27 (close) trigger")
+		sleep(5)
+
+	mutex.release()
+	return
+
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/pin17', methods=['POST'])
 def pin17():
-	pin17 = LED(17)
-	pin17.on()
-	print("pin17 (open) trigger")
-	sleep(5)
+	control(1)
 	return '', 200
 
 @app.route('/pin27', methods=['POST'])
 def pin27():
-	pin27 = LED(27)
-	pin27.on()
-	print("pin27 (close) trigger")
-	sleep(5)
+	control(-1)
 	return '', 200
 
 @app.route('/temper', methods=['GET'])
