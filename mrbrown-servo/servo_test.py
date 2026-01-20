efa073ab1853d6147c017eba34f684faefa410e1
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2026-01-19 22:00:20 -0800

    Auto-sync from pi07: Mon Jan 19 22:00:18 PST 2026

--- mrbrown-servo/servo_test.py
+++ mrbrown-servo/servo_test.py
@@ -8,7 +8,12 @@ kit = ServoKit(channels=16)
 servo_num = 15
 
 # Move from 0 to 100 degrees gradually
-for angle in range(0, 101, 5):
+for angle in range(0, 180, 5):
+    kit.servo[servo_num].angle = angle
+    print(f"Moving servo {servo_num} to {angle}°")
+    time.sleep(0.2)
+
+for angle in range(180, 0, -5):
     kit.servo[servo_num].angle = angle
     print(f"Moving servo {servo_num} to {angle}°")
     time.sleep(0.2)
