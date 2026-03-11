b35c70697dfe1250dfb75b1321e500779ed3c551
slvusd-gitbot <ebrown+gitbot@slvusd.org>
2026-03-10 17:20:05 -0700

    Auto-sync from pi07: Tue Mar 10 17:20:04 PDT 2026

--- gavin/gfpressuresensor.py
+++ gavin/gfpressuresensor.py
@@ -1,3 +1,35 @@
 import ms5837
-sensor.read(ms5837.OSR_256)
+import time
+
+"""
+sensor = ms5837.MS5837_02ba()
+
+if not sensor.init():
+	print("Sensor could not be Initialized")
+	exit(1)
+
+
+while True:
+	if sensor.read():
+		print(("P: %0.1f mbar 
+
+"""
+sensor = ms5837.MS5837_30BA()  # Change to MS5837_02BA if using Bar02
+
+
+if not sensor.init():
+    print("Sensor could not be initialized")
+    exit(1)
+
+
+while True:
+    if sensor.read():
+        print(("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
+            sensor.pressure(),
+            sensor.pressure(ms5837.UNITS_psi),
+            sensor.temperature(),
+            sensor.temperature(ms5837.UNITS_Farenheit)))
+    else:
+        print("Sensor read failed!")
+        exit(1)
 
